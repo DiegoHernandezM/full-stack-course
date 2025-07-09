9ebb892124b89d85deee90cc0852c03ed00e2fc6
Diego Hernandez <dhernandezm@iMac-de-Diego.local>
2025-07-09 11:12:36 -0600

    exercises 1.3, 1.4 and 1.5

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,4 +1,5 @@
 const Header = (props) => {
+  console.log(props)
   return (
     <div>
       <h1>{props.course}</h1>
@@ -26,18 +27,26 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content part={part1}/>
-      <Total total={exercises1} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
