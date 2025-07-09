8afaae5f4f8a3d92ec3b8990913cd2a4ce34405b
Diego Hernandez <dhernandezm@iMac-de-Diego.local>
2025-07-09 10:59:23 -0600

    exercises 1.1 and 1.2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,37 +1,43 @@
-import './App.css'
-
-const Hello = (props) => {
+const Header = (props) => {
   return (
     <div>
-      <p>Hello {props.name}, you are {props.age} years old</p>
+      <h1>{props.course}</h1>
     </div>
   )
 }
 
-const Footer = () => {
+const Content = (props) => {
   return (
     <div>
-      greeting app created by <a href='https://github.com/mluukkai'>mluukkai</a>
+      <Part part={props.part} />
+      <Part part={props.part} /> 
+      <Part part={props.part} />
     </div>
   )
 }
 
-function App() {
-  const now = new Date();
-  const a = 10;
-  const b = 20;
-  const name = 'Diego';
-  const age = 33;
-  console.log(now, a+b);
+const Total = (props) => {
+  return (
+    <div>
+      <p>{props.total}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name={name} age={age}/>
-      <Hello name='George' age={20 + 10}/>
-      <p>It is {now.toString()}</p>
-      <p>plus {b} is {a+b}</p>
-      <Footer />
+      <Header course={course} />
+      <Content part={part1}/>
+      <Total total={exercises1} />
     </div>
   )
 }
